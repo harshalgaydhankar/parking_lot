2577eca8e6da60c6be72c6e3c512e1a5ef841c45
harshalgaydhankar <harshal.gaydhankar@axisbank.com>
2021-06-24 15:30:10 +0530

    Harshal | add park functionality to parking lot

--- src/model/ParkingLot.js
+++ src/model/ParkingLot.js
@@ -1,8 +1,32 @@
 const Slot = require('./Slot');
+const Status = require("../constants/Status");
+const Car = require("./Car");
 
 class ParkingLot {
     constructor(numberOfSlots) {
         this.slots = [...new Array(numberOfSlots)].map((item, index)=> new Slot(index+1));
+        console.log(`Created parking lot with ${numberOfSlots} slots`)
+    }
+
+    park(carRegistrationNumber){
+        const availableSlotNumber = this.availableSubsequentSlot();
+        if(availableSlotNumber === -1){
+            console.log("Sorry, parking lot is full");
+        }
+        else{
+            this.slots[availableSlotNumber - 1].allocate(new Car(carRegistrationNumber));
+        }
+    }
+
+    availableSubsequentSlot(){
+        const availableSlots = this.slots.filter(slot => slot.status === Status.VACANT);
+        if(availableSlots.length > 0){
+            availableSlots.sort((a, b) => a.slotNumber - b.slotNumber);
+            return availableSlots[0].slotNumber;
+        }
+        else {
+            return -1;
+        }
     }
 }
 
--- test/model/ParkingLot.test.js
+++ test/model/ParkingLot.test.js
@@ -2,7 +2,22 @@ const {ParkingLot} = require('../../src/model');
 
 describe('ParkingLot', function () {
     it("gets initialized with given number of slots", function () {
+        console.log = jest.fn();
         const parkingLot = new ParkingLot(3);
         expect(parkingLot.slots.length).toBe(3);
+        expect(console.log).toBeCalledTimes(1)
+        expect(console.log).toHaveBeenCalledWith('Created parking lot with 3 slots');
     });
+
+    it("parks car on next available subsequent slot", function () {
+        const parkingLot = new ParkingLot(3);
+        let nextSubsequentSlotNumber = parkingLot.availableSubsequentSlot();
+        parkingLot.park("KA-09-HH-0987");
+        expect(
+            parkingLot.slots.find(
+                (slot) => slot.car.registrationNumber === "KA-09-HH-0987").slotNumber
+        ).toBe(nextSubsequentSlotNumber);
+
+    });
+
 });
